c98052e0e34d2d1be110188e055548b2b416a157
Mousakaa <arthur.gaudard@gmx.fr>
2023-10-09 15:34:40 +0200

    butterfly approximation changes

--- butterfly.vhd
+++ butterfly.vhd
@@ -22,33 +22,27 @@ entity butterfly is
 end entity;
 
 architecture compute of butterfly is
-	signal wrAr, wrAi, wrBr, wrBi, wiAr, wiAi, wiBr, wiBi: std_logic_vector(2*l+1 downto 0); -- (1;2*l+2;l)
+	signal wrAr, wrAi, wrBr, wrBi, wiAr, wiAi, wiBr, wiBi: integer range 0 to 2**(2*l+2)-1; --std_logic_vector(2*l+1 downto 0); -- (1;2*l+2;l)
+	signal S2r_full, S2i_full: std_logic_vector(2*l+3 downto 0); -- (1;2*l+4;l)
 begin
 	-- S1 = A + B
 	S1r <= std_logic_vector(to_signed(to_integer(signed(Ar)) + to_integer(signed(Br)), l+1));
 	S1i <= std_logic_vector(to_signed(to_integer(signed(Ai)) + to_integer(signed(Bi)), l+1));
 
 	-- S2 = w(A + B)
-	wrAr <= std_logic_vector(to_signed(to_integer(signed(wr)) * to_integer(signed(Ar)), 2*l+2));
-	wrAi <= std_logic_vector(to_signed(to_integer(signed(wr)) * to_integer(signed(Ai)), 2*l+2));
-	wrBr <= std_logic_vector(to_signed(to_integer(signed(wr)) * to_integer(signed(Br)), 2*l+2));
-	wrBi <= std_logic_vector(to_signed(to_integer(signed(wr)) * to_integer(signed(Bi)), 2*l+2));
-	wiAr <= std_logic_vector(to_signed(to_integer(signed(wi)) * to_integer(signed(Ar)), 2*l+2));
-	wiAi <= std_logic_vector(to_signed(to_integer(signed(wi)) * to_integer(signed(Ai)), 2*l+2));
-	wiBr <= std_logic_vector(to_signed(to_integer(signed(wi)) * to_integer(signed(Br)), 2*l+2));
-	wiBi <= std_logic_vector(to_signed(to_integer(signed(wi)) * to_integer(signed(Bi)), 2*l+2));
+	wrAr <= to_integer(signed(wr)) * to_integer(signed(Ar));
+	wrAi <= to_integer(signed(wr)) * to_integer(signed(Ai));
+	wrBr <= to_integer(signed(wr)) * to_integer(signed(Br));
+	wrBi <= to_integer(signed(wr)) * to_integer(signed(Bi));
+	wiAr <= to_integer(signed(wi)) * to_integer(signed(Ar));
+	wiAi <= to_integer(signed(wi)) * to_integer(signed(Ai));
+	wiBr <= to_integer(signed(wi)) * to_integer(signed(Br));
+	wiBi <= to_integer(signed(wi)) * to_integer(signed(Bi));
 
-	S2r <= std_logic_vector(to_signed(
-		   to_integer(signed(wrAr(2*l-n+1 downto l-n))) + to_integer(signed(std_logic_vector('0' & wrAr(l-n-1)))) -- Approximation
-		   - to_integer(signed(wrBr(2*l-n+1 downto l-n))) + to_integer(signed(std_logic_vector('0' & wrBr(l-n-1)))) -- Approximation
-		   - to_integer(signed(wiAi(2*l-n+1 downto l-n))) + to_integer(signed(std_logic_vector('0' & wiAi(l-n-1)))) -- Approximation
-		   + to_integer(signed(wiBi(2*l-n+1 downto l-n))) + to_integer(signed(std_logic_vector('0' & wiBi(l-n-1)))) -- Approximation
-		   , l+1));
+	S2r_full <= std_logic_vector(to_signed(wrAr - wrBr - wiAi + wiBi, 2*l+4));
+	S2i_full <= std_logic_vector(to_signed(wrAi - wtBi + wiAr - wiBr, 2*l+4));
 
-	S2i <= std_logic_vector(to_signed(
-		   to_integer(signed(wrAi(2*l-n+1 downto l-n))) + to_integer(signed(std_logic_vector('0' & wrAi(l-n-1)))) -- Approximation
-		   - to_integer(signed(wrBi(2*l-n+1 downto l-n))) + to_integer(signed(std_logic_vector('0' & wrBi(l-n-1)))) -- Approximation
-		   + to_integer(signed(wiAr(2*l-n+1 downto l-n))) + to_integer(signed(std_logic_vector('0' & wiAr(l-n-1)))) -- Approximation
-		   - to_integer(signed(wiBr(2*l-n+1 downto l-n))) + to_integer(signed(std_logic_vector('0' & wiBr(l-n-1)))) -- Approximation
-		   , l+1));
+	-- Approximation on l+1 bits with precision n
+	S2r <= std_logic_vector(signed(S2r_full(2*l-n+1 downto l-n)) + signed("0" & S2r_full(l-n-1)));
+	S2i <= std_logic_vector(signed(S2i_full(2*l-n+1 downto l-n)) + signed("0" & S2i_full(l-n-1)));
 end architecture;
